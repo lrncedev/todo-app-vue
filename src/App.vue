c08aa3826de36cde7d12529d86f2258493673c83
Laurence Cadag <laurencecads1@gmail.com>
2022-10-28 22:51:22 +0800

    Initial Commit: Project Start

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,85 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h1>Note Application</h1>
+    <div class="note-input">
+      <input type="text" name="note-text" id="note-text" placeholder="Title of the Note">
+      <button id="note-btn" @click="add">Add</button>
+    </div>
+    <div class="note-list">
+      <div class="note" v-for="note in notes" :key="note">
+      {{ note }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      notes: ['Test', 'Test2', 'test3']
+    }
+  },
+  methods: {
+    add() {
+      this.notes.push('etasd');
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700;900&display=swap');
+
+
+*,
+*::before,
+*::after {
+  padding: 0;
+  margin: 0;
+  line-height: 1.2;
+  list-style: none;
+  text-decoration: none;
 }
+:root {
+  font-family: 'Roboto', sans-serif;
+  --font-weight-light: 100;
+  --font-weight-black: 900;
+}
+.container {
+  height: 100vh;
+}
+.container, .note-input{
+  width: 60%;
+  margin-inline: auto;
+
+  h1 {
+    text-align: center;
+    font-weight: var(--font-weight-black);
+    font-size: 3rem;
+    margin-bottom: .5em;
+  }
+
+  .note-input {
+    display: flex;
+    margin-bottom: 2em;
+
+    #note-text {
+      flex-grow: 1;
+    }
+
+    #note-btn {
+      border: none;
+      font-weight: var(--font-weight-light);
+      padding: .5em;
+      background-color: green;
+      color: white;
+    }
+  }
+}
+
 </style>
