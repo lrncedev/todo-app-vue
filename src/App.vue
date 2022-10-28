97f72e773d43758cc148dfabc43566292470e26c
Laurence Cadag <laurencecads1@gmail.com>
2022-10-28 23:33:29 +0800

    2nd Commit: Note template and SCSS Styles

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,16 @@
       <button id="note-btn" @click="add">Add</button>
     </div>
     <div class="note-list">
-      <div class="note" v-for="note in notes" :key="note">
-      {{ note }}</div>
+      <div 
+        v-for="note in notes" 
+        :key="note" 
+        class="note"
+      >
+        <div class="note-header">
+          <h2 class="note-title">{{ note }}</h2>
+          <button class="close">x</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -21,6 +29,16 @@ export default {
   },
   data() {
     return {
+      colorArray:  ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6', 
+        '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
+        '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A', 
+        '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
+        '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC', 
+        '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
+        '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680', 
+        '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
+        '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3', 
+        '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'],
       notes: ['Test', 'Test2', 'test3']
     }
   },
@@ -51,12 +69,6 @@ export default {
   --font-weight-black: 900;
 }
 .container {
-  height: 100vh;
-}
-.container, .note-input{
-  width: 60%;
-  margin-inline: auto;
-
   h1 {
     text-align: center;
     font-weight: var(--font-weight-black);
@@ -67,7 +79,8 @@ export default {
   .note-input {
     display: flex;
     margin-bottom: 2em;
-
+    width: 60%;
+    margin-inline: auto;
     #note-text {
       flex-grow: 1;
     }
@@ -79,6 +92,38 @@ export default {
       background-color: green;
       color: white;
     }
+  }
+
+  .note-list {
+    padding: 3em 2em;
+    height: minmax(67vh, 100vh);
+    display: grid;
+    gap: 1em;
+    grid-template-columns: minmax(1fr, 1fr);
+
+    .note {
+      min-height: 200px;
+
+      .note-header{
+        display: flex;
+        justify-content: space-between;
+
+        .close {
+          padding: .2em .6em;
+          border: none;
+          background-color: red;
+          border-radius: 3px;
+          color: white;
+          font-family: var(--font-weight-black)
+        }
+      }
+    }
+  }
+}
+
+@media only screen and (min-width: 30em){
+  .note-list {
+    grid-template-columns: repeat(4, minmax(50px, 1fr));
   }
 }
 
