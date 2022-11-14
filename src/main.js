aa344165cdf88ceed74193407b032e3a8bebc8e6
Laurence Cadag <laurencecads1@gmail.com>
2022-11-14 14:36:22 +0800

    Commit 21: JS files modified for routing and FontAwesome library

--- src/main.js
+++ src/main.js
@@ -2,5 +2,23 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import router from './router'; 
+import { library } from '@fortawesome/fontawesome-svg-core'
 
-createApp(App).use(router).mount('#app')
+/* import font awesome icon component */
+import { FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+/* FREE BRANDS SVG */
+
+// import { faHtml5, faCss3, faJs, faVuejs, faGit, faSass, faFacebookF, faLinkedinIn, faCodepen, faTwitter, faGithub, faNodeJs, faNode, faPhp} from '@fortawesome/free-brands-svg-icons'
+
+/* FREE SOLID SVG */
+import { faBars, faSquareCheck, faExclamationCircle, faTasks, faHome, faPlusCircle, faDownload} from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+
+// library.add( faHtml5, faCss3, faJs, faVuejs, faGit, faSass, faFacebookF, faLinkedinIn, faCodepen, faTwitter,faBars ,faGithub, faNodeJs, faNode, faPhp, faSquareCheck)
+
+library.add(faSquareCheck, faBars, faExclamationCircle, faTasks, faHome, faPlusCircle, faDownload)
+// createApp(App).use(router).mount('#app')
+createApp(App).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,37 @@
 import { createWebHistory, createRouter } from "vue-router";
 
 /* Views directory */
-import Home from '@/views/HomePage.vue'
-import TaskList from '@/views/TaskList.vue'
+import Home from '@/views/HomePage.vue';
+import TaskList from '@/views/TaskList.vue';
+
+/* Nested Children Components */
+import TaskView from '@/components/TaskView.vue';
+import AccomplishedTask from '@/components/AccomplishedTask.vue';
+import ImportantTask from '@/components/ImportantTask.vue';
 const routes = [
   { 
     path: '/', 
     component: Home 
   },
   { 
-    path: '/list', 
-    component: TaskList 
+    path: '/task', 
+    name: 'TaskView',
+    component: TaskList,
+    children: [
+      { path: '', name: 'NewTask', component: TaskView },
+      { path: '/important', name: 'ImportantTask', component: ImportantTask },
+      { path: '/accomplished', name: 'AccomplishedTask', component: AccomplishedTask }
+    ],
   },
+  // { 
+  //   path: '/important', 
+  //   component: TaskList,
+  //   children: [
+  //     { path: '', name: '', component: ImportantTask },
+  //     // { path: 'test', name: 'Home', component: Home}
+  //   ],
+  // },
 ]
-
 
 const router = createRouter( {
   history: createWebHistory(),
